4452b405a0d6164028d8cf72e643dcd0186b5b12
shadowrunner11 <pieroramirez810@gmail.com>
2023-02-19 22:33:29 -0500

    :pencil: changed config deploy

--- src/ts/utils/index.ts
+++ src/ts/utils/index.ts
@@ -1,1 +1,10 @@
 export { $ } from './selectors'
+
+export function prefixWithBase(href: string){
+  const baseUrl = import.meta.env.BASE_URL
+
+  if(!baseUrl)
+    return href
+
+  return `/${baseUrl}${href}`
+}
