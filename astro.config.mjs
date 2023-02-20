537b59711af58a4117809c0fa2083f82797dc1b8
shadowrunner11 <pieroramirez810@gmail.com>
2023-02-19 22:22:56 -0500

    :pencil: changed config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+const isGithubActions = process.env.GITHUB_ACTIONS
 // https://astro.build/config
 export default defineConfig({
+  ...(isGithubActions? { 
+    site: 'https://astronaut.github.io', 
+    base: '/bootcampLanding'
+  } : {}),
   integrations: [tailwind()]
 });
