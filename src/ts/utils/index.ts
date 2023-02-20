58040833942d4aa299557b9ca9bfa59fa7ac4146
shadowrunner11 <pieroramirez810@gmail.com>
2023-02-19 22:39:30 -0500

    :pencil: changed config deploy

--- src/ts/utils/index.ts
+++ src/ts/utils/index.ts
@@ -1,10 +1,9 @@
 export { $ } from './selectors'
 
 export function prefixWithBase(href: string){
-  const baseUrl = import.meta.env.BASE_URL
 
-  if(!baseUrl)
+  if(!import.meta.env.PROD)
     return href
 
-  return `/${baseUrl}${href}`
+  return `/bootcampLanding${href}`
 }
